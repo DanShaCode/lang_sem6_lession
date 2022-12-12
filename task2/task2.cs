cb30eacfd135861dca7ce0c755823b4aee3d3ed9
Danis Sharipov <danshadance@mail.ru>
2022-12-13 00:34:11 +0600

    Задача 40 решена

---
+++ task2/task2.cs
@@ -0,0 +1,10 @@
+﻿// Переворот массива 
+
+int [] nums = {1, 2, 3 ,4 ,5};
+
+int length = nums.Length;
+for (int i = length - 1; i >= 0; i--)
+{
+    Console.Write($"{nums[i]} ");
+}
+
---
+++ task3/task3.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine();
+Console.Write("Введите цифру 3: ");
+
+int userData = Convert.ToInt32(Console.ReadLine());
+if (userData < 3 | userData > 3) Console.WriteLine("Ошибка ввода. Введите цифру 3.");
+else
+{
+int [] nums = new int [userData];
+
+void FillArray (int [] num)
+{
+    int length = num.Length;
+    int index = 0;
+    for (index = 0; index < length; index++)
+    {
+        num[index] = new Random().Next(1,10);
+    }
+}
+
+void PrintArray (int [] nummss)
+{
+    int leng = nummss.Length;
+    int i = 0;
+    Console.WriteLine();
+    Console.Write("Сформированный массив из чисел: ");
+    for (i = 0; i < leng; i++)
+    {
+        Console.Write($"{nummss[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FillArray(nums);
+PrintArray(nums);
+Console.WriteLine();
+
+string yes = "Да. Можно построить треугольник.";
+string no = "Нет. Построить треугольник нельзя.";
+
+if (nums[0] < nums[1] + nums[2]
+ && nums[1] < nums[2] + nums[0]
+ && nums[2] < nums[1] + nums[0])
+{
+    Console.WriteLine(yes);
+}
+else Console.WriteLine(no);
+}
